09f2135b2d0cecb43a37d5c46f3364d8e76b22cd
Yash-p <fatpotatoes.serveyash@gmail.com>
2015-10-09 12:56:54 -0500

    Made the snake go faster the more apples it got

--- BaseSnake.py
+++ BaseSnake.py
@@ -33,7 +33,7 @@ clock = pygame.time.Clock()
 # This loop is very interesting. When will it stop running?
 # (hint- when is the while condition false?)
 while True:
-    clock.tick(10)
+    clock.tick(10+score/2)
 
     # This gets the keyboard input. Don't worry too much about the first couple lines.
     for keypress in pygame.event.get():
